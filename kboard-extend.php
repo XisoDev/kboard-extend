3ac63503d282df4ffc25a7c0f6e4ef1da58331ac
xiso <ceo@amuz.co.kr>
2025-02-22 21:01:58 +0900

    first commit

---
+++ kboard-extend.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Kboard Extend
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: xiso
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
